284387c434a0282a87ff7ec42a61d2afebefb3a0
Codemaine <jermaine.antwi@icloud.com>
2022-08-10 19:28:26 +0000

    added an indication for amount earned, fixed table error and added date

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
-
-file = open('output.csv', 'w')
-writer = csv.writer(file)
+from datetime import date
+import math
 
 print('Enter start time (Format hh:mm in 24hrs):')
 x = input()
@@ -17,5 +16,18 @@ if x and y:
     f = fy-fx
     s = max(sx, sy) - min(sx, sy) * 0.834
     fr = f * 5 + s
-    writer.writerow(['Starting Time', 'Ending Time', 'Amount'])
-    writer.writerow([x, y, fr])
+    print("You earned $"+str(math.trunc(round(fr, 0)))+" for tutoring for "+str(math.trunc(round(f, 0)))+" hours and "+str(math.trunc(round(s, 0)))+" minutes")
+    List = [x, y, date.today(), fr]
+    # Open our existing CSV file in append mode
+    # Create a file object for this file
+    with open('output.csv', 'a') as f_object:
+        # Pass this file object to csv.writer()
+        # and get a writer object
+        writer_object = csv.writer(f_object)
+
+        # Pass the list as an argument into
+        # the writerow()
+        writer_object.writerow(List)
+
+        # Close the file object
+        f_object.close()
